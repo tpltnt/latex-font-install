ec164224447e9e26d10e75291b18edf4240526f5
tpltnt <tpltnt.github@dropcut.net>
2013-05-18 16:50:39 +0300

    commandline check added

--- install-latex-fontfile.sh
+++ install-latex-fontfile.sh
@@ -5,4 +5,9 @@
 # licensed under the AGPLv3
 # http://www.gnu.org/licenses/agpl-3.0.html
 
+if [ "$#" -ne 1 ] || ! [ -d "$1" ]; then
+  echo "Usage: $0 LATEXFONTFILE.zip" >&2
+  exit 1
+fi
+
 unzip $1 -d /usr/local/share/texmf/ && update-updmap && mktexlsr && updmap-sys 
