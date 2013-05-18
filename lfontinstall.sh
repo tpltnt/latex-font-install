1562a42cbac8e34f05d311636a6b5a4d2927dc43
tpltnt <tpltnt.github@dropcut.net>
2013-05-18 15:36:33 +0200

    Update lfontinstall.sh

--- lfontinstall.sh
+++ lfontinstall.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+# a small shell script to install LaTeX fonts
+# written by tpltnt, 2013
+# licensed under the AGPLv3
+# http://www.gnu.org/licenses/agpl-3.0.html
+
 unzip $1 -d /usr/local/share/texmf/ && update-updmap && mktexlsr && updmap-sys 
