da9c226afedd7da0b6ee7ade9cf9ade42560ae8c
tpltnt <tpltnt.github@dropcut.net>
2013-05-18 16:51:44 +0300

    typo fixed

--- install-latex-fontfile.sh
+++ install-latex-fontfile.sh
@@ -5,7 +5,7 @@
 # licensed under the AGPLv3
 # http://www.gnu.org/licenses/agpl-3.0.html
 
-if [ "$#" -ne 1 ] || ! [ -d "$1" ]; then
+if [ "$#" -ne 1 ] || ! [ -f "$1" ]; then
   echo "Usage: $0 LATEXFONTFILE.zip" >&2
   exit 1
 fi
