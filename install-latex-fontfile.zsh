5e8e82943499318af4fec1b8dde8355775ecc9c2
tpltnt <tpltnt.github@dropcut.net>
2013-05-18 15:51:22 +0200

    cmdline arg check added

--- install-latex-fontfile.zsh
+++ install-latex-fontfile.zsh
@@ -1,5 +1,9 @@
 # zsh function defintion
 
 install-latex-fontfile() {
+  if [ "$#" -ne 1 ] || ! [ -f "$1" ]; then
+    echo "Usage: $0 LATEXFONTFILE.zip" >&2
+    exit 1
+  fi
   unzip $1 -d /usr/local/share/texmf/ && update-updmap && mktexlsr && updmap-sys
 }
